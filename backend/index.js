02a3a63895aca5d48b6a64fef9fa8cfd073fd406
Jason Gong <jasongong1@outlook.com>
2022-03-25 00:47:02 +1030

    post request to /tasks to add a task now returns object with newId field for new task's id, moved mongo connection string to .env

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,18 @@ const cors = require("cors");
 const express = require("express");
 const fs = require("fs");
 const bodyParser = require('body-parser');
+const { env } = require('process')
+require('dotenv').config()
 
 const app = express();
-const port = 8000;
-const saveFile = 'tasks.json'
+const PORT = 8000;
+const MONGO_CONNECTION_STRING = env.MONGO_CONNECTION_STRING;
 
-const credentials = '.certificate'
+const CREDENTIALS_PATH = '.certificate'
 
-const client = new MongoClient('<CONNECTION STRING HERE>', {
-  sslKey: credentials,
-  sslCert: credentials,
+const client = new MongoClient(MONGO_CONNECTION_STRING, {
+  sslKey: CREDENTIALS_PATH,
+  sslCert: CREDENTIALS_PATH,
   serverApi: ServerApiVersion.v1
 });
 
@@ -32,8 +34,10 @@ async function run() {
 
     app.post('/tasks', async (req, res) => {
         const newTask = req.body;
-        await tasksCollection.insertOne(newTask);
-        res.sendStatus(200);
+        await tasksCollection.insertOne(newTask, (e) => {
+            if (e) return;
+        });
+        res.status(200).send({newId: newTask._id});
     })
 
     app.delete('/tasks', async (req, res) => {
@@ -44,6 +48,6 @@ async function run() {
 }
 
 run().catch(console.dir).then(() => {
-    app.listen(port, () => console.log(`Server started on port ${port}`))
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 });
 
