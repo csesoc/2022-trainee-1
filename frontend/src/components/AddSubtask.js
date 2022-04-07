8c1ba98ce212cef9b6b82eb4ada423e85714a3da
sssijin <sijin8828@gmail.com>
2022-04-07 16:10:16 +1000

    changed add button for add subtask and a few other minor styling things

--- frontend/src/components/AddSubtask.js
+++ frontend/src/components/AddSubtask.js
@@ -1,10 +1,8 @@
-import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import DateTimePicker from '@mui/lab/DateTimePicker'; 
-import TextField from '@mui/material/TextField';
-import styled from 'styled-components';
-import CategoryTags from './CategoryTags';
+import React, { useState } from "react";
+import styled from "styled-components";
+import CategoryTags from "./CategoryTags";
+import Button from "@mui/material/Button";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
 
 export const AddTaskContainer = styled.div`
     margin: 30px auto;
@@ -13,43 +11,59 @@ export const AddTaskContainer = styled.div`
     width: 40%;
     border-radius: 10px;
     background-color: rgba(162, 210, 255, 0.1);
-`
+`;
 
-const AddSubtask = ({ task, onAddSubtask, closeForm, calculateNewProgress }) => {
-    const [subtaskText, setSubtaskText] = useState('')
+const AddSubtask = ({
+    task,
+    onAddSubtask,
+    closeForm,
+    calculateNewProgress,
+}) => {
+    const [subtaskText, setSubtaskText] = useState("");
 
     const [subtasks, setSubtasks] = useState(task.subtasks);
     //const [editSubtask, setEditSubtask] = useState(task.subtasks);
-    
+
     const onSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         closeForm();
         const subtaskMap = new Map();
-        subtaskMap.set("task", subtaskText)
-        subtaskMap.set("complete", false)
-        setSubtasks([...subtasks, subtaskMap])
+        subtaskMap.set("task", subtaskText);
+        subtaskMap.set("complete", false);
+        setSubtasks([...subtasks, subtaskMap]);
 
         // subtasks now includes the subtask we just added
-        onAddSubtask(task.id, [...subtasks, subtaskMap])
-        
-        console.log(task.subtasks.length)
+        onAddSubtask(task.id, [...subtasks, subtaskMap]);
+
+        console.log(task.subtasks.length);
         calculateNewProgress();
         // Clear form
-        setSubtaskText('')
-    }
+        setSubtaskText("");
+    };
 
     return (
-        <AddTaskContainer className='AddTaskContainer'>
+        <AddTaskContainer className="AddTaskContainer">
             <form onSubmit={onSubmit}>
                 <div className="form-group">
-                    <input className="task-text" placeholder="add subtask" type="text" value={subtaskText} onChange={(e) => setSubtaskText(e.target.value)}/>
+                    <input
+                        className="task-text"
+                        placeholder="Add Subtask"
+                        type="text"
+                        value={subtaskText}
+                        onChange={(e) => setSubtaskText(e.target.value)}
+                    />
                 </div>
-
-                <button type="submit">add</button>
+                <br></br>
+                <Button
+                    variant="outlined"
+                    type="submit"
+                    startIcon={<ModeEditIcon />}
+                >
+                    Add
+                </Button>
             </form>
         </AddTaskContainer>
-        
-    )
-}
+    );
+};
 
-export default AddSubtask
+export default AddSubtask;
